23f88576b863407e838cd63f1602f1cc0e583b42
Ivan Kulinich <kulinichvanya@gmail.com>
2020-03-12 01:15:56 +0200

    Updates

---
+++ precache-manifest.89a5ef5f32bca10ea46941bbee976670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d2306f3040700fad2d60987aca837d",
+    "url": "/funny_chess_quiz/index.html"
+  },
+  {
+    "revision": "ade91e895d946a713947",
+    "url": "/funny_chess_quiz/static/js/2.f60ca78f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/funny_chess_quiz/static/js/2.f60ca78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac1508523652e798828",
+    "url": "/funny_chess_quiz/static/js/main.40acd6e8.chunk.js"
+  },
+  {
+    "revision": "599a33b55023988bca80",
+    "url": "/funny_chess_quiz/static/js/runtime-main.2d22d664.js"
+  }
+]);
